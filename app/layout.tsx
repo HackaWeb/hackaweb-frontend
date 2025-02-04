b19c0476e26aa291cbea368cb3a194c441e3d678
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-05 00:12:09 +0200

    _

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
+import { ReduxProvider } from "@/components/providers/Redux";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -24,10 +26,13 @@ const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
     return (
         <html lang="en">
             <body className={inter.variable}>
-                {/* <Aside />
+                <ReduxProvider>
+                    {/* <Aside />
                 <Header /> */}
-                <main>{children}</main>
-                {/* <Footer /> */}
+                    <main>{children}</main>
+                    {/* <Footer /> */}
+                    <ToastContainer />
+                </ReduxProvider>
             </body>
         </html>
     );
