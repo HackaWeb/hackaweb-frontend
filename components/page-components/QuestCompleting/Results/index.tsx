b58c046d28997b81fe359e31b515f4da145a572b
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-08 17:57:48 +0200

    code fixes

--- components/page-components/QuestCompleting/Results/index.tsx
+++ components/page-components/QuestCompleting/Results/index.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/Button";
-import Link from "next/link";
-import { IoTrophyOutline } from "react-icons/io5";
 import { MakeReview } from "./MakeReview";
+import { GeneralResults } from "./GeneralResults";
+import { Actions } from "./Actions";
 
 export const Results = () => {
     return (
@@ -18,52 +17,8 @@ export const Results = () => {
                 </h1>
             </div>
             <div className="mt-6 grid grid-cols-3 gap-6 px-20 items-start">
-                <div className="bg-blackOpacity p-6 rounded-lg">
-                    <div className="text-xl font-bold">Ваші результати</div>
-                    <ul className="mt-4 pb-2 border-b-2 border-gray border-opacity-10 relative text-lg">
-                        <IoTrophyOutline className="absolute right-0 bottom-0 size-9 text-purple" />
-                        <li>
-                            Відсоток правильних відповідей:{" "}
-                            <span className="text-purple">95</span> / 100 %
-                        </li>
-                        <li className="mt-1">
-                            Час проходження:{" "}
-                            <span className="text-purple">35</span> хв
-                        </li>
-                        <li className="mt-1">
-                            Місце в рейтингу тесту:{" "}
-                            <span className="text-purple">3</span> з 20
-                        </li>
-                    </ul>
-                    <ul className="flex gap-3 mt-4">
-                        <li className="text-yellow p-1 px-2 border-2 border-purple rounded-md">
-                            Топ 3
-                        </li>
-                        <li className="text-yellow p-1 px-2 border-2 border-purple rounded-md">
-                            Спрінтер
-                        </li>
-                        <li className="text-yellow p-1 px-2 border-2 border-purple rounded-md">
-                            Макс бал
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <Link href="#" className="flex justify-center">
-                        <Button color="purpleBackground">
-                            Спробувати ще раз
-                        </Button>
-                    </Link>
-                    <Link href="/" className="flex justify-center">
-                        <Button color="purpleBorder" className="mt-3">
-                            Повернутися на головну
-                        </Button>
-                    </Link>
-                    <Link href="#" className="flex justify-center">
-                        <Button color="yellowBorder" className="mt-3">
-                            Пройти інший квест цього автора
-                        </Button>
-                    </Link>
-                </div>
+                <GeneralResults />
+                <Actions />
                 <MakeReview />
             </div>
         </div>
