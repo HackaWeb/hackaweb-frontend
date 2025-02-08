e7901353137c10ef2d6ec392b5b21cc84a2e9dc3
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-08 12:50:24 +0200

    quest general info

--- app/quests/[id]/page.tsx
+++ app/quests/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { GeneralInfo } from "@/components/page-components/Quest/GeneralInfo";
 import { Leaderboard } from "@/components/page-components/Quest/Leaderboard";
 import { Reviews } from "@/components/page-components/Quest/Reviews";
+import { Button } from "@/components/ui/Button";
 import { Quest } from "@/types/quest.interface";
+import Link from "next/link";
+import { TbArrowBackUp } from "react-icons/tb";
 
 const quest: Quest = {
     id: "1",
@@ -9,7 +12,7 @@ const quest: Quest = {
     description: "Description 1",
     timesPlayed: 30,
     rating: 4,
-    imageUrl: "/quest.png",
+    imageUrl: "/test.png",
     owner: {
         id: "1",
         nickname: "Danil Diachenko",
@@ -24,7 +27,13 @@ const QuestDetails = () => {
     return (
         <div>
             <h1>Деталі квесту</h1>
-            <div className="grid grid-cols-[0.33fr_0.66fr]">
+            <Link href="#" className="block mt-4">
+                <Button color="purpleBorder">
+                    <TbArrowBackUp className="size-6" />
+                    <span>Повернутися назад</span>
+                </Button>
+            </Link>
+            <div className="grid grid-cols-[0.33fr_0.66fr] mt-6">
                 <div>
                     <GeneralInfo quest={quest} />
                     <Reviews quest={quest} />
