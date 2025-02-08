838732e315218e420752ad2107da5f0e705cda4e
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-08 17:51:42 +0200

    make review

--- components/page-components/QuestCompleting/Results/index.tsx
+++ components/page-components/QuestCompleting/Results/index.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
 import { IoTrophyOutline } from "react-icons/io5";
+import { MakeReview } from "./MakeReview";
 
 export const Results = () => {
     return (
@@ -44,6 +47,24 @@ export const Results = () => {
                         </li>
                     </ul>
                 </div>
+                <div>
+                    <Link href="#" className="flex justify-center">
+                        <Button color="purpleBackground">
+                            Спробувати ще раз
+                        </Button>
+                    </Link>
+                    <Link href="/" className="flex justify-center">
+                        <Button color="purpleBorder" className="mt-3">
+                            Повернутися на головну
+                        </Button>
+                    </Link>
+                    <Link href="#" className="flex justify-center">
+                        <Button color="yellowBorder" className="mt-3">
+                            Пройти інший квест цього автора
+                        </Button>
+                    </Link>
+                </div>
+                <MakeReview />
             </div>
         </div>
     );
