75238054a19fdccdcb9f0d4d95ee4e2772399898
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-09 22:02:27 +0200

    Update user.ts

---
+++ api/user.ts
@@ -0,0 +1,31 @@
+import { fetchApi } from "./fetchApi.api";
+import { UpdateProfileRequestBody } from "./requestBodies/profile.interface";
+import {
+    DeleteUserResponse,
+    GetProfileResponse,
+    UpdateUserResponse,
+} from "./responses/user.types";
+
+export const getProfile = async (): Promise<GetProfileResponse> =>
+    fetchApi({
+        endpoint: "/profile/",
+        isAuthRequired: true,
+        method: "GET",
+    });
+
+export const updateUserProfile = async (
+    body: UpdateProfileRequestBody,
+): Promise<UpdateUserResponse> =>
+    fetchApi({
+        endpoint: "/User/update-profile/",
+        isAuthRequired: true,
+        method: "PUT",
+        body: body,
+    });
+
+export const deleteUserProfile = async (): Promise<DeleteUserResponse> =>
+    fetchApi({
+        endpoint: "/profile/",
+        isAuthRequired: true,
+        method: "DELETE",
+    });
