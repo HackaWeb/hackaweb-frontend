c5395c6a243380808d37a398d2148c8fe6d90eab
Danil <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-11 14:54:33 +0200

    Update index.tsx

--- components/Modals/index.tsx
+++ components/Modals/index.tsx
@@ -1,8 +1,8 @@
 import { CreateQuest } from "./CreateQuest";
 import { CreateQuestion } from "./CreateQuestion";
 import { DeleteConfirmation } from "./DeleteConfirmation";
-import QuestEdit from "../modals/QuestEdit";
-import QuestionEdit from "../modals/QuestionEdit";
+import QuestEdit from "./Modals/QuestEdit";
+import QuestionEdit from "./Modals/QuestionEdit";
 
 export const Modals = () => {
     return (
