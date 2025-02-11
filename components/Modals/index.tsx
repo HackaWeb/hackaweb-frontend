8a3e0f49d84e7d8e77d913843fa4a9253526c4de
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-11 14:56:54 +0200

    modals fix

--- components/Modals/index.tsx
+++ components/Modals/index.tsx
@@ -1,8 +1,8 @@
 import { CreateQuest } from "./CreateQuest";
 import { CreateQuestion } from "./CreateQuestion";
 import { DeleteConfirmation } from "./DeleteConfirmation";
-import QuestEdit from "./Modals/QuestEdit";
-import QuestionEdit from "./Modals/QuestionEdit";
+import QuestEdit from "./QuestEdit";
+import QuestionEdit from "./QuestionEdit";
 
 export const Modals = () => {
     return (
