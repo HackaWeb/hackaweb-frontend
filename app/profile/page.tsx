7c503f58fc3942e231951c41009001c6bb8f8db6
Ivan Dutov <ivann.dutov@gmail.com>
2025-02-11 21:53:57 +0200

    everything works

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,17 @@
 import { getCompletedQuestsByOwnerId, getQuestsByOwnerId } from "@/api/quests";
+import { DEFAULT_FIELD_ERROR } from "@/api/responses/common/failure.interface";
 import { getMyProfile } from "@/api/user";
 import { MyProfilePageComponent } from "@/components/page-components/MyProfile";
 import { getCookie } from "@/helpers/getCookie";
-import { Quest } from "@/types/quest.interface";
-import { Profile } from "@/types/user.interface";
 import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 const MyProfile = async () => {
     const token = await getCookie("token");
+
+    if (!token) {
+        redirect("/login");
+    }
 
     const getProfile = async () => {
         try {
@@ -25,60 +29,39 @@ const MyProfile = async () => {
         }
     };
 
-    const getCompletedQuests = async () => {
-        /* try {
-            const res = fetch(
-                `https://hackawebquiz.ashycoast-bbbe20af.westus2.azurecontainerapps.io/api/quests/completed/${"3577b43d-8d91-4cd3-9c4c-280c38490d2a"}`,
-                {},
-            )
-                .then((res) => console.log(res))
-                .then((data) => {
-                    console.log(data);
-                });
-        } catch (error) {
-            console.error(error);
-        } */
-        try {
-            const res = await fetch(
-                `https://hackawebquiz.ashycoast-bbbe20af.westus2.azurecontainerapps.io/api/quiz/completed/3577b43d-8d91-4cd3-9c4c-280c38490d2a`,
-            );
-            console.log(await res.json());
-        } catch (er) {}
-        /* const data = await getCompletedQuestsByOwnerId(profile.id);
-        console.log(data);
-        
-        return data; */
-    };
-    getCompletedQuests();
-
     const getOwnQuests = async () => {
         try {
-            /* const data = await getQuestsByOwnerId(profile.id); */
-            const res = await fetch(
-                `https://hackawebquiz.ashycoast-bbbe20af.westus2.azurecontainerapps.io/api/quiz/3577b43d-8d91-4cd3-9c4c-280c38490d2a`,
-            );
-            console.log("own", await res.json());
-            /* console.log(data); */
-
-            /* return data.items; */
+            const data = await getQuestsByOwnerId(profile.id);
+            return data.quizzes;
         } catch (error) {
             console.error(error);
+            return [];
         }
     };
 
-    if (!token) {
+    const getCompletedQuests = async () => {
+        try {
+            return await getCompletedQuestsByOwnerId();
+        } catch (error) {
+            console.error(error);
+            toast.error(DEFAULT_FIELD_ERROR.message);
+            return [];
+        }
+    };
+
+    const profile = await getProfile();
+    if (!profile) {
         redirect("/login");
     }
 
-    const profile = await getProfile();
     const ownQuests = await getOwnQuests();
     const completedQuests = await getCompletedQuests();
 
     return (
         <MyProfilePageComponent
             profile={profile}
-            ownQuests={[]}
-            completedQuests={[]}
+            ownQuests={ownQuests}
+            completedQuests={completedQuests}
         />
     );
 };
