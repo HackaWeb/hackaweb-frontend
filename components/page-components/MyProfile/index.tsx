a7e96044a6d068271207f13a10e90f4fc9c2d179
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-12 09:22:03 +0200

    profile adaptive

--- components/page-components/MyProfile/index.tsx
+++ components/page-components/MyProfile/index.tsx
@@ -11,17 +11,19 @@ export const MyProfilePageComponent = ({
     ownQuests,
 }: MyProfileProps) => {
     return (
-        <div className="mt-8 ml-8 md:ml-0">
+        <div className="mt-8">
             <h1>Мій кабінет</h1>
             <ReturnBtn className="mt-4" />
-            <div className="mt-8 grid grid-cols-1 sm:grid-cols-[240px_auto] 2xl:grid-cols-[240px_330px_auto] gap-6 items-start">
-                <LeftColumn
-                    profile={profile}
-                    completedQuests={completedQuests}
-                    ownQuests={ownQuests}
-                />
+            <div className="mt-8 grid grid-cols-[1fr] sm:grid-cols-[240px_auto] 2xl:grid-cols-[240px_330px_auto] gap-6 items-start">
+                <div>
+                    <LeftColumn
+                        profile={profile}
+                        completedQuests={completedQuests}
+                        ownQuests={ownQuests}
+                    />
+                </div>
                 <MiddleColumn profile={profile} />
-                <div className="grid-cols-1 grid items-start -col-start-3 -col-end-1 2xl:col-start-auto 2xl:col-end-auto">
+                <div className="grid-cols-1 grid items-start sm:-col-start-3 sm:-col-end-1 2xl:col-start-auto 2xl:col-end-auto">
                     <OwnQuests
                         ownQuests={ownQuests}
                         profile={profile}
