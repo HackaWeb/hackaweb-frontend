de3d942e82137166a86332d8acbb3006d69f79ab
Ivan Dutov <ivann.dutov@gmail.com>
2025-02-12 10:31:15 +0200

    import

--- app/quests/[id]/page.tsx
+++ app/quests/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getLeaderboardByQuestId } from "@/api/leaderboard";
 import { getQuestById } from "@/api/quests";
 import { GeneralInfo } from "@/components/page-components/Quest/GeneralInfo";
 import { Leaderboard } from "@/components/page-components/Quest/Leaderboard";
-import { Reviews } from "@/components/page-components/Quest/Reviews";
+import { Reviews } from "@/components/page-components/Quest/Reews";
 import { Button } from "@/components/ui/Button";
 import { LeaderboardUser, Quest } from "@/types/quest.interface";
 import Link from "next/link";
